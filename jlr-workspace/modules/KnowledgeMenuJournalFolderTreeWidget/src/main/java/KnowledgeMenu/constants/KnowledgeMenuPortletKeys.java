d6476e977adb17a2fa4534a1147280e195a33a85
nimishagopalakrishnan <nimishaaa.g@gmail.com>
2021-08-13 12:21:24 +0100

    Initial commit

---
+++ jlr-workspace/modules/KnowledgeMenuJournalFolderTreeWidget/src/main/java/KnowledgeMenu/constants/KnowledgeMenuPortletKeys.java
@@ -0,0 +1,11 @@
+package KnowledgeMenu.constants;
+
+/**
+ * @author carlos
+ */
+public class KnowledgeMenuPortletKeys {
+
+	public static final String KNOWLEDGEMENU =
+		"KnowledgeMenu_KnowledgeMenuPortlet";
+
+}
